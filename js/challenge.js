6f9665febade517d2727238e0eae3b94a10184ed
JCGuest <jcguest90@gmail.com>
2020-03-05 13:42:05 -0600

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -10,39 +10,23 @@ function incrementCount() {
 function stopTimer() {
     clearInterval(intervalId);
     intervalId = null;
-}
+};
 
 function startTimer() {
     intervalId = setInterval(incrementCount, 1000)
-}
-startTimer()
+};
+startTimer();
 
 let pause = document.getElementById("pause");
 pause.addEventListener('click', function(){
     if (intervalId) {
         stopTimer();
-        pause.innerText = "play";
+        pause.innerText = " play ";
     } else {
         startTimer();
         pause.innerText = "pause";
     };
 });
-
-function newLi() {
-    return document.createElement("li");
-};
-
-function getOccurrence() {
-    var count = 0;
-    likedAry.forEach((v) => (v === counter.innerText && count++));
-    return count;
-};
-
-let likes = document.querySelector(".likes");
-function printLike(counterText) {
-    let newLike = likes.appendChild(newLi());
-    newLike.innerText = `${counterText} has been liked ${getOccurrence()} times`;
-};
 
 let minus = document.querySelector("#minus");
 minus.addEventListener("click", function() { 
@@ -54,6 +38,22 @@ plus.addEventListener("click", function() {
     counter.innerText = parseInt(counter.innerText, 10) + 1;
 });
 
+function getOccurrence() {
+    var count = 0;
+    likedAry.forEach((v) => (v === counter.innerText && count++));
+    return count;
+};
+
+function newLi() {
+    return document.createElement("li");
+};
+
+let likes = document.querySelector(".likes");
+function printLike(counterText) {
+    let newLike = likes.appendChild(newLi());
+    newLike.innerText = `${counterText} has been liked ${getOccurrence()} times`;
+};
+
 let likedAry = [];
 let heart = document.querySelector("#heart");
 heart.addEventListener("click", function() {
@@ -63,7 +63,6 @@ heart.addEventListener("click", function() {
 });
 
 let commentForm = document.getElementsByTagName("form")[0];
-
 commentForm.addEventListener("submit", function(a){
     a.preventDefault();
     var b=this.children[0]
@@ -71,9 +70,5 @@ commentForm.addEventListener("submit", function(a){
     var d=document.querySelector(".comments")
     e=document.createElement("p");
     e.innerText=c,d.appendChild(e)
-});
-
-heart.addEventListener("click", function() {
-    var time = counter.innerText 
 });
 });
