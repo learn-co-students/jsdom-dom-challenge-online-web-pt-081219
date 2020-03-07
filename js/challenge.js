86070455e6f96bc4f78bc73c05f976b7e1829a72
JCGuest <jcguest90@gmail.com>
2020-03-06 22:57:44 -0600

    added  addOrCreateLi() to  fully satisfy all the requirements for the lab

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 let intervalId = null;
-let counter = document.querySelector("#counter");
+const counter = document.querySelector("#counter");
 function incrementCount() {
     const current_count = parseInt(counter.innerText);
     counter.innerText = current_count + 1;
@@ -38,26 +38,30 @@ plus.addEventListener("click", function() {
     counter.innerText = parseInt(counter.innerText, 10) + 1;
 });
 
+let likes = document.querySelector(".likes");
+function addOrCreateLi(counterText) {
+    if (document.querySelector(`body > ul > li:nth-child(${counterText})`)) {
+        let updateLi = document.querySelector(`body > ul > li:nth-child(${counterText})`)
+        updateLi.innerText = `${counterText} has been liked ${getOccurrence()} times`;
+    } else {
+    let newLike = likes.appendChild(document.createElement('li'));
+    newLike.innerText = `${counterText} has been liked ${getOccurrence()} times`;
+    };
+};
+
+let likedAry = [];
+let heart = document.querySelector("#heart");
+heart.addEventListener("click", function() {
+    const counterText = parseInt(counter.innerText);
+    likedAry.push(`${counterText}`);
+    addOrCreateLi(counterText);
+});
+
 function getOccurrence() {
     var count = 0;
     likedAry.forEach((v) => (v === counter.innerText && count++));
     return count;
 };
-
-let likes = document.querySelector(".likes");
-function newLi(counterText) {
-    let newLike = likes.appendChild(document.createElement('li'));
-    newLike.innerText = `${counterText} has been liked ${getOccurrence()} times`;
-};
-
-
-let likedAry = [];
-let heart = document.querySelector("#heart");
-heart.addEventListener("click", function() {
-    let counterText = parseInt(counter.innerText);
-    likedAry.push(`${counterText}`);
-    newLi(counterText);
-});
 
 let commentForm = document.getElementsByTagName("form")[0];
 commentForm.addEventListener("submit", function(a){
